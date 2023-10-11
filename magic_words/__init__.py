00daad08cbb6805364c7417cee7a0c840248cbca
Aman Bhargava <amanb2000@gmail.com>
2023-10-11 22:57:48 +0000

    Add package skeleton

---
+++ magic_words/__init__.py
@@ -0,0 +1,13 @@
+from magic_words.compute_score import compute_score
+from magic_words.batch_compute_score import batch_compute_score
+from magic_words.prompt_hack import greedy_prompt_hack
+from magic_words.search_limiters import SearchLimiter, BruteForce
+from magic_words.prompt_hack_qa import greedy_prompt_hack_qa
+
+# Gumbel softmax
+# from magic_words.gumbel_softmax_main import gumbel_prompt_hack, TauScheduler
+
+# Greedy Coordinate Gradient
+# from magic_words.coordinate_descent import gcg_prompt_hack
+# from magic_words.coordinate_descent_qa import gcg_prompt_hack_qa
+from magic_words.easy_gcg import easy_gcg_qa, easy_gcg_qa_ids
