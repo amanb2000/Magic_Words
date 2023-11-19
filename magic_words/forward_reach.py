02768f6b6326bc22d46b3aa84b66cffb15acee96
Aman Bhargava <amanb2000@gmail.com>
2023-11-19 08:04:48 +0000

    Write + test _get_answer_ids() function

--- magic_words/forward_reach.py
+++ magic_words/forward_reach.py
@@ -19,6 +19,33 @@ def _get_prompt_ids_brute_force(tokenizer):
     return prompt_ids
 
 
+def _get_answer_ids(prompt_ids, question_ids, model, tokenizer): 
+    """ Runs `[prompt_ids + question_ids]` through the model to get logits, then 
+    takes the argmax over the vocab_size dimension to return the set of 
+    `answer_ids` as a [batch, 1] tensor. 
+
+    `prompt_ids` has shape [batch, prompt_length]
+    `question_ids` has shape [batch, question_length]
+    """
+    if question_ids.shape[0] == 1 and prompt_ids.shape[0] > 1:
+        question_ids = question_ids.repeat(prompt_ids.shape[0], 1)
+
+    assert prompt_ids.shape[0] == question_ids.shape[0], 'Batch dimension of prompt_ids and question_ids must match in _get_answer_ids()'
+
+    # Compute the logits 
+    input_ids = torch.cat([prompt_ids, question_ids], dim=-1).to(model.device)
+    with torch.no_grad():
+        logits = model(input_ids).logits
+    
+    # Compute the answer_ids
+    answer_ids = logits[:, -1, :].argmax(dim=-1).unsqueeze(-1)
+
+    # ensure answer_ids has the right dimensions 
+    assert answer_ids.shape == (prompt_ids.shape[0], 1), f"answer_ids has shape {answer_ids.shape} but should have shape {(prompt_ids.shape[0], 1)}"
+
+    return answer_ids
+
+
 def get_reachable_set(x_0, model, tokenizer, max_prompt_tokens=10):
     """ Given a single state x_0, generate the reachable set of y* values by 
     enumerating all possible prompts u of length less than max_prompt_tokens. 
--- tests/test_forward_reach.py
+++ tests/test_forward_reach.py
@@ -10,6 +10,7 @@ import transformers
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 from magic_words.forward_reach import _get_prompt_ids_brute_force
+from magic_words.forward_reach import _get_answer_ids
 
 
 class TestForwardReach(unittest.TestCase): 
@@ -27,20 +28,20 @@ class TestForwardReach(unittest.TestCase):
         cls.tokenizer = AutoTokenizer.from_pretrained(cls.model_name, padding_side="left")
         cls.tokenizer.pad_token = cls.tokenizer.eos_token
 
-        # cls.pipeline = transformers.pipeline(
-        #     "text-generation",
-        #     model=cls.model_name,
-        #     tokenizer=cls.tokenizer,
-        #     torch_dtype=torch.bfloat16,
-        #     trust_remote_code=True,
-        #     device_map="auto",
-        # )
+        cls.pipeline = transformers.pipeline(
+            "text-generation",
+            model=cls.model_name,
+            tokenizer=cls.tokenizer,
+            torch_dtype=torch.bfloat16,
+            trust_remote_code=True,
+            device_map="auto",
+        )
 
-        # cls.model = cls.pipeline.model
-        # cls.model.eval()
+        cls.model = cls.pipeline.model
+        cls.model.eval()
 
-        # cls.device = cls.model.device
-        # print("Model device: ", cls.device)
+        cls.device = cls.model.device
+        print("Model device: ", cls.device)
 
     def setUp(self): 
         """This is run before every single individual test method. 
@@ -58,5 +59,18 @@ class TestForwardReach(unittest.TestCase):
 
     def test_brute_force(self): 
         prompt_ids = _get_prompt_ids_brute_force(self.tokenizer)
-        pdb.set_trace()
         print("Prompt_ids shape: ", prompt_ids.shape)
+
+    def test_get_answer(self): 
+        question = "What is the meaning of life?"
+        question_ids = self.tokenizer(question, return_tensors="pt")["input_ids"]
+
+        prompt_ids = _get_prompt_ids_brute_force(self.tokenizer)
+
+        # select the first 300 
+        prompt_ids = prompt_ids[:300, :]
+
+        answer_ids = _get_answer_ids(prompt_ids, question_ids, self.model, self.tokenizer)
+
+        print("Answer_ids shape: ", answer_ids.shape)
+        print("Answer_ids: ", answer_ids)
