1589d2f8d9417b091ca7f89517aeaee8f6937c42
Cameron <cwitkowski@rogers.com>
2024-06-25 20:44:47 -0400

    Added all layers functionality

--- theorem_numerics/ripser_script.py
+++ theorem_numerics/ripser_script.py
@@ -14,6 +14,7 @@ def parse_args():
     parser.add_argument('--input_file', type=str, help='Path to input file')
     parser.add_argument('--layer', type=int, help='Layer to analyze')
     parser.add_argument('--output_file', type=str, default=None, help='Path to output file. Do not include extension.')
+    parser.add_argument('--all_layers', type=bool, help='Analyze all layers (overrides --layer)')
 
     args = parser.parse_args()
 
@@ -31,6 +32,17 @@ def load_jsonl(file_path, layer_num):
 
     # Extract the final token representations
     final_token_reps = torch.tensor([item["final_token_rep"][layer_num] for item in data])
+
+    return final_token_reps
+
+def load_jsonl_all(file_path):
+    data = []
+    with open(file_path, "r") as file:
+        for line in file:
+            data.append(json.loads(line))
+
+    # Extract the final token representations
+    final_token_reps = [torch.tensor([item["final_token_rep"][layer_num] for item in data]) for layer_num in range(33)]
 
     return final_token_reps
 
@@ -75,6 +87,9 @@ def plot_diagram(diagram, output_file=None):
 
     if output_file is not None:
         plt.savefig(output_file + ".png")
+        print(f"Saved plot to {output_file}.png")
+        # Clear the plot
+        plt.clf()
     else:
         plt.show()
 
@@ -82,21 +97,38 @@ def plot_diagram(diagram, output_file=None):
 if __name__ == '__main__':
     args = parse_args()
 
-    # Load data
-    data = load_jsonl(args.input_file, args.layer)
+    if args.all_layers:
+        data = load_jsonl_all(args.input_file)
+        layer_i = 0
+        for data_layer in data:
+            # Count connected components
+            epsilon = 200
+            num_components = 2
+            while num_components > 1:
+                num_components, diagram_h0 = count_connected_components(data_layer, epsilon)
+                epsilon *= 2
 
+            plot_diagram(diagram_h0, output_file=args.output_file + f"_layer{layer_i}")
 
+            # Save diagram to np file if output file is provided
+            if args.output_file is not None:
+                np.save(args.output_file + f"_layer{layer_i}.npy", diagram_h0)
+                print(f"Saved diagram to {args.output_file}_layer{layer_i}.npy")
+            layer_i += 1
+    else:
+        # Load data
+        data = load_jsonl(args.input_file, args.layer)
 
-    # Count connected components
-    epsilon = 200
-    num_components = 2
-    while num_components > 1:
-        num_components, diagram_h0 = count_connected_components(data, epsilon)
-        epsilon *= 2
+        # Count connected components
+        epsilon = 200
+        num_components = 2
+        while num_components > 1:
+            num_components, diagram_h0 = count_connected_components(data, epsilon)
+            epsilon *= 2
 
-    plot_diagram(diagram_h0, output_file=args.output_file)
-    
-    # Save diagram to np file if output file is provided
-    if args.output_file is not None:
-        np.save(args.output_file + ".npy", diagram_h0)
-        print(f"Saved diagram to {args.output_file}.npy")
+        plot_diagram(diagram_h0, output_file=args.output_file)
+        
+        # Save diagram to np file if output file is provided
+        if args.output_file is not None:
+            np.save(args.output_file + ".npy", diagram_h0)
+            print(f"Saved diagram to {args.output_file}.npy")
